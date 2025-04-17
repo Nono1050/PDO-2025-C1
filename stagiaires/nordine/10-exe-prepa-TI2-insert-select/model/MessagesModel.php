39776c361f718ce700b20948d0e8ca905d64bde0
Nono1050 <abdelkadernordine@hotmail.com>
2025-04-17 13:16:23 +0200

    exo10

--- stagiaires/nordine/10-exe-prepa-TI2-insert-select/model/MessagesModel.php
+++ stagiaires/nordine/10-exe-prepa-TI2-insert-select/model/MessagesModel.php
@@ -1,4 +1,21 @@
 <?php
+function getTeLastArticles(PDO $myDb): array|string
+{
+try {
+    $result = $myDb->query(query:'
+        SELECT title , titlte_slug, 'text', article_date_create
+        FROM article
+        ORDER BY article_date_create ASC
+        LIMIT 4
+        ');
+        // pas de resultat
+        if($result->rowCont() === 0) return "Pas encore d'articles"; // (string)
+        // on retoune 
+}catch (Exeption $e){
+    return $e-> getMessage();
+}
+
+}
 # fonctions en lien avec la table article
 
 # chargement des articles classés par `create_date` DESC
--- stagiaires/nordine/10-exe-prepa-TI2-insert-select/public/index.php
+++ stagiaires/nordine/10-exe-prepa-TI2-insert-select/public/index.php
@@ -8,7 +8,20 @@
 # chargement des constantes de connexion en mode prod
 require_once "../config.php";
 # chargement du modèle (fonctions)
-
+try {
+    $db = new PDO(
+        DB_CONNECT_TYPE.":host=".
+        DB_CONNECT_HOST.";dbname=".
+        DB_CONNECT_NAME.";charset=".
+        DB_CONNECT_CHARSET,
+        DB_CONNECT_USER,
+        DB_CONNECT_PWD,
+    );
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(Exception $e){
+    // arrêt du script et affichage de l'erreur de connexion
+    die("Code erreur : {$e->getCode()} | Message : {$e->getMessage()}");
+}
 
 # connexion à PDO
 
